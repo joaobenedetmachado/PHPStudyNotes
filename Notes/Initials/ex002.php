0a769deb3bcbefa84e5d73e2a7e87f4ade47b7eb
Joao Victor Benedet Machado <jvbm2177@gmail.com>
2025-02-20 17:11:29 -0300

    exercicios

---
+++ Notes/Initials/ex002.php
@@ -0,0 +1,8 @@
+<?php
+$nome = $_POST['name'];
+$endereco = $_POST['endereco'];
+$bairro = $_POST['bairro'];
+$cidade = $_POST['cidade'];
+
+echo "Nome: " . $nome . ", Endereco: " . $bairro . ", Cidade: " . $cidade;
+?>
