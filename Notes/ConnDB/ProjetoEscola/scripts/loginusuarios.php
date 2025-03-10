a571c91f4b4c557729e485edb2f80e2b11c77356
Joao Victor Benedet Machado <joao.193922@alunosatc.edu.br>
2025-03-10 15:17:16 -0300

    parte de login e tal

---
+++ Notes/ConnDB/ProjetoEscola/scripts/loginusuarios.php
@@ -0,0 +1,24 @@
+<?php 
+$connect = mysql_connect("localhost", "root" ,"");
+$banco = mysql_select_db("escola");
+
+if (isset($_POST['logincurso'])) {
+
+    $login = $_POST['login'];
+    $senha = $_POST['senha'];
+
+    $sql = "SELECT id FROM usuarios WHERE login = '$login' and senha = '$senha';";
+
+    $resultado = mysql_query($sql);
+    
+    if ($resultado == TRUE) {
+        echo "deu certo, usuario encontrado " . $resultado;
+        header("location:../home.html");
+        setcookie("login",$login)
+        exit(); 
+    } else {
+        echo "deu errado" . mysql_error();
+    }
+}
+
+?>
