e13dd606ed360f844acf72965998253cf1b5d355
jvbm2177@gmail.com <jvbm2177@gmail.com>
2025-08-12 14:56:24 -0300

    added cors

--- Notes/pokeapi/backend.php
+++ Notes/pokeapi/backend.php
@@ -1,6 +1,9 @@
 <?php
     // Tipo de resposta = JSON
     header('Content-Type: application/json');
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
     // Resposta padrão do backend: 
     $response = array(
